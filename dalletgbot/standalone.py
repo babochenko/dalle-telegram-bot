ce1ce963f952f3d17d07ae4b06dab1fa383a9b56
denisbabochenko <babochenko.denis2@gmail.com>
2023-03-01 18:05:08 +0000

    fix gcloud deployment

---
+++ dalletgbot/standalone.py
@@ -0,0 +1,19 @@
+from flask import Flask, Response
+from flask import request
+
+from main import generate_images
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def handle_request():
+    msg = request.get_json()
+    print('msg', msg)
+
+    generate_images(msg)
+    return Response('ok', status=200)
+
+
+if __name__ == '__main__':
+    app.run(port=5002, debug=True)
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 gcloud functions deploy dalle-tg-bot \
   --runtime=python311 \
   --region=europe-west3 \
+  --source=./dalletgbot/ \
   --entry-point=generate_images \
   --env-vars-file .env.yaml \
   --trigger-http \
